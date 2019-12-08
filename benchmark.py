02db16f6c009aa7022277f40419ac5dcc8cafca4
Tille12 <jens.tilmanrau@googlemail.com>
2019-12-08 20:59:28 +0100

    added print-outs and small changes

--- benchmark.py
+++ benchmark.py
@@ -16,6 +16,7 @@ def benchmark_ga(benchmark_filename, input_graph_name, number_of_graphs,
         input_string = input_string_1 + " " + input_string_2 + " " + input_string_3
         # Concatenate command string and run command
         command_string = commandstring_before_input + input_string + commandstring_after_input + benchmark_filename
+        print(command_string)
         subprocess.run(command_string, shell=True)
 
 
@@ -47,87 +48,88 @@ commandstring_before_input = "python3 GraphDating.py -i "   # TODO: CHANGE TO "p
 commandstring_after_input = ""  # depends on chosen algorithm
 
 # ### CREATE RANDOM GRAPHS FOR DATASET SMALL, MIDDLE AND BIG
+print("###\t0\t### Create random graphs for subsequent graph alignments")
 # # Number of graphs has to be divisible by three (three input graphs per alignment) without rest
 # ## Small random graphs
 # number_of_small_graphs = 90 # TODO: Set number
 # input_graph_name_small = "rg_small_4_0.4"   # TODO: Set name
 # create_random_graphs(4, 0.4, False, number_of_small_graphs, input_graph_name_small)
-# ## Middle random graphs
-# number_of_middle_graphs = 60    # TODO: Set number
-# input_graph_name_middle = "rg_middle_8_0.4" # TODO: Set name
-# create_random_graphs(8, 0.4, False, number_of_middle_graphs, input_graph_name_middle)
+## Middle random graphs
+# number_of_middle_graphs = 3    # TODO: Set number
+# input_graph_name_middle = "rg_middle_6_1.0" # TODO: Set name
+# create_random_graphs(6, 1.0, False, number_of_middle_graphs, input_graph_name_middle)
 # ## Big random graphs
-# number_of_big_graphs = 30   # TODO: Set number
-# input_graph_name_big = "rg_big_8_0.4" # TODO: Set name
-# create_random_graphs(12, 0.4, False, number_of_middle_graphs, input_graph_name_big)
-#
-# ### GRAPH ALIGNMENT: BRON-KERBOSCH
-# commandstring_after_input = " -ga bk -bm "
+number_of_big_graphs = 3   # TODO: Set number
+input_graph_name_big = "rg_big_7_0.5" # TODO: Set name
+create_random_graphs(7, 0.5, False, number_of_big_graphs, input_graph_name_big)
+
+### GRAPH ALIGNMENT: BRON-KERBOSCH
+commandstring_after_input = " -ga bk -bm "
 # ##  Kleiner Datensatz
+# print("###\t1.1\t### Benchmark: Graph-Alignment, Bron-Kerbosch, size small")
 # benchmark_filename = "benchmark_ga_bk_small.txt"
 # benchmark_ga(benchmark_filename, input_graph_name_small, number_of_small_graphs,
 #              commandstring_before_input, commandstring_after_input)
 #
 # ##  Mittlerer Datensatz
+# print("###\t1.2\t### Benchmark: Graph-Alignment, Bron-Kerbosch, size middle")
 # benchmark_filename = "benchmark_ga_bk_middle.txt"
 # benchmark_ga(benchmark_filename, input_graph_name_middle, number_of_middle_graphs,
 #              commandstring_before_input, commandstring_after_input)
 #
 # ##  Grosser Datensatz
-# benchmark_filename = "benchmark_ga_bk_big.txt"
-# benchmark_ga(benchmark_filename, input_graph_name_big, number_of_middle_graphs,
-# ### GRAPH ALIGNMENT: CORDELLA
-# ##  Kleiner Datensatz
-#
-# ##  Mittlerer Datensatz
-#
-# ##  Grosser Datensatz
-#              commandstring_before_input, commandstring_after_input)
+print("###\t1.3\t### Benchmark: Graph-Alignment, Bron-Kerbosch, size big")
+benchmark_filename = "benchmark_ga_bk_big.txt"
+benchmark_ga(benchmark_filename, input_graph_name_big, number_of_big_graphs,
+             commandstring_before_input, commandstring_after_input)
 #
 #
 # ### GRAPH ALIGNMENT: Cordella
-# commandstring_after_input = " -ga mb -bm "
+commandstring_after_input = " -ga mb -bm "
 # ##  Kleiner Datensatz
+# # print("###\t2.1\t### Benchmark: Graph-Alignment, Cordella, size small")
 # benchmark_filename = "benchmark_ga_mb_small.txt"
 # benchmark_ga(benchmark_filename, input_graph_name_small, number_of_small_graphs,
 #              commandstring_before_input, commandstring_after_input)
 #
 # ##  Mittlerer Datensatz
+# print("###\t2.2\t### Benchmark: Graph-Alignment, Cordella, size middle")
 # benchmark_filename = "benchmark_ga_mb_middle.txt"
 # benchmark_ga(benchmark_filename, input_graph_name_middle, number_of_middle_graphs,
 #              commandstring_before_input, commandstring_after_input)
 #
-# ##  Grosser Datensatz
-# benchmark_filename = "benchmark_ga_mb_big.txt"
-# benchmark_ga(benchmark_filename, input_graph_name_big, number_of_middle_graphs,
-#              commandstring_before_input, commandstring_after_input)
+##  Grosser Datensatz
+print("###\t2.3\t### Benchmark: Graph-Alignment, Cordella, size big ###")
+benchmark_filename = "benchmark_ga_mb_big.txt"
+benchmark_ga(benchmark_filename, input_graph_name_big, number_of_big_graphs,
+             commandstring_before_input, commandstring_after_input)
+
+
+
+# ### CREATE RANDOM GRAPHS FOR Bron-Kerbosch    
+# number_of_graphs = 5
+# repeats_per_graph = 3
+# size_of_graphs = 20
+# connectivity = 0.5
+# input_graph_name_bk = "rg_bk_50_0.5"
+# create_random_graphs(size_of_graphs, connectivity, False, number_of_graphs, input_graph_name_bk)
 #
-
-
-### CREATE RANDOM GRAPHS FOR Bron-Kerbosch
-number_of_graphs = 5
-repeats_per_graph = 3
-size_of_graphs = 20
-connectivity = 0.5
-input_graph_name_bk = "rg_bk_50_0.5"
-create_random_graphs(size_of_graphs, connectivity, False, number_of_graphs, input_graph_name_bk)
-
-## Bron-Kerbosch: Pivot random
-commandstring_after_input = " -bk -p random -bm "
-benchmark_filename = "benchmark_bk_p_random.txt"
-for i in range(1, number_of_graphs + 1):
-    input_graph_string = input_graph_name_bk + "_" + str(i) + ".graph"
-    benchmark_bk(benchmark_filename, input_graph_string, repeats_per_graph,
-                 commandstring_before_input, commandstring_after_input)
-    with open(benchmark_filename, 'a') as file:
-        file.write("\n")
-
-## Bron-Kerbosch: Pivot max
-commandstring_after_input = " -bk -p max -bm "
-benchmark_filename = "benchmark_bk_p_max.txt"
-for i in range(1, number_of_graphs + 1):
-    input_graph_string = input_graph_name_bk + "_" + str(i) + ".graph"
-    benchmark_bk(benchmark_filename, input_graph_string, repeats_per_graph,
-                 commandstring_before_input, commandstring_after_input)
-    with open(benchmark_filename, 'a') as file:
-        file.write("\n")
+# ## Bron-Kerbosch: Pivot random
+# commandstring_after_input = " -bk -p random -bm "
+# benchmark_filename = "benchmark_bk_p_random.txt"
+# for i in range(1, number_of_graphs + 1):
+#     input_graph_string = input_graph_name_bk + "_" + str(i) + ".graph"
+#     benchmark_bk(benchmark_filename, input_graph_string, repeats_per_graph,
+#                  commandstring_before_input, commandstring_after_input)
+#     with open(benchmark_filename, 'a') as file:
+#         file.write("\n")
+#
+# ## Bron-Kerbosch: Pivot max
+# commandstring_after_input = " -bk -p max -bm "
+# benchmark_filename = "benchmark_bk_p_max.txt"
+# for i in range(1, number_of_graphs + 1):
+#     input_graph_string = input_graph_name_bk + "_" + str(i) + ".graph"
+#     benchmark_bk(benchmark_filename, input_graph_string, repeats_per_graph,
+#                  commandstring_before_input, commandstring_after_input)
+#     with open(benchmark_filename, 'a') as file:
+#         file.write("\n")
