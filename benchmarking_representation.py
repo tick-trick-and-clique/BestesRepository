d0105c0c05466126bbf07ca0812623674817054b
AJ-Felix <AJ-Felix@users.noreply.github.com>
2019-12-14 13:38:51 +0100

    python file der grafiken

--- benchmarking_representation.py
+++ benchmarking_representation.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def stats_from_array_data(filename, x_value):
+def stats_from_array_data_ga(filename, x_value):
     fopen = open(filename, "r")
     means = []
     stds = []
@@ -24,21 +24,52 @@ def stats_from_array_data(filename, x_value):
     return x_value, means, stds, n, failed
 
 
-def build_bar_plot(x, y, e, n, title, ylim, anno_height, ylabel):
+def stats_from_array_data_bk(filename, x_value):
+    fopen = open(filename, "r")
+    values = []
+    for line in fopen.readlines():
+        line = line.rstrip(";")
+        list = line.split(";")
+        i = float(list[0])
+        values.append(i)
+    ar = np.array(values)
+    return ar.mean(), ar.std(), x_value
+
+
+def build_bar_plot_ga(x, y, e, n, title, ylim, anno_height, ylabel):
     fig = plt.figure()
     plt.bar(x, y)
-    print(e)
-    plt.errorbar(x, y, yerr=[np.zeros(len(e)), e], linestyle='None', elinewidth=1, capsize=3, )
-    plt.xlabel("#Nodes")
-    plt.ylabel(ylabel)
+    plt.errorbar(x, y, yerr=[np.zeros(len(e)), e], linestyle='None', elinewidth=1, capsize=3)
+    plt.xlabel("Number of Nodes", labelpad=12)
+    plt.ylabel(ylabel, labelpad=12)
     axes = plt.gca()
     axes.set_ylim([0, ylim])
     i = 0
     for a in x:
         label = str(n[i])
+        if i < 4:
+            label += "/30"
+        else:
+            label += "/10"
         i += 1
         plt.annotate(label, (a, anno_height), textcoords="offset points", xytext=(0, 10), ha='center', color="r")
     plt.suptitle(title)
+    return fig
+
+
+def build_bar_plot_bk(x, y, e, ylim, anno_height):
+    fig = plt.figure(figsize=(3.0, 4.8))
+    plt.bar(x, y, width=0.7)
+    plt.errorbar(x, y, yerr=[np.zeros(len(e)), e], linestyle='None', elinewidth=1, capsize=3)
+    plt.xlabel("Pivot Type", labelpad=13)
+    plt.xlim(-0.8, 1.8)
+    plt.ylabel("Time [ms]", labelpad=13)
+    axes = plt.gca()
+    axes.set_ylim([0, ylim])
+    for a in x:
+        plt.annotate("30/30", (a, anno_height), textcoords="offset points", xytext=(0, 10), ha='center', color="r")
+    plt.suptitle("Pivoting (Bron-Kerbosch)")
+    plt.tight_layout(rect=(0, 0, 1, 0.90))
     return fig
 
 
@@ -50,48 +81,50 @@ def data_for_plot_from_stats_ga(list_of_stats):
     failed = [i[4] for i in list_of_stats]
     return x, y, e, n, failed
 
-def data_for_plot_from_stats_bk(stats):
-    x, y, e, n, failed = stats
-    for i in range(len(y)):
-        rel_err = e[i]/y[i]
-    return
+
+def data_for_plot_from_stats_bk(list_of_stats):
+    x = [i[2] for i in list_of_stats]
+    y = [i[0] for i in list_of_stats]
+    e = [i[1] for i in list_of_stats]
+    return x, y, e
 
 
 if __name__ == '__main__':
 
     # Build plot for graph alignment using bron-kerbosch
-    stat3 = stats_from_array_data("Benchmarking\GraphAlignment\\benchmark_ga_bk_3_0.5.txt", 3)
-    stat4 = stats_from_array_data("Benchmarking\GraphAlignment\\benchmark_ga_bk_4_0.5.txt", 4)
-    stat5 = stats_from_array_data("Benchmarking\GraphAlignment\\benchmark_ga_bk_5_0.5.txt", 5)
-    stat6 = stats_from_array_data("Benchmarking\GraphAlignment\\benchmark_ga_bk_6_0.5.txt", 6)
-    stat7 = stats_from_array_data("Benchmarking\GraphAlignment\\benchmark_ga_bk_7_0.5.txt", 7)
+    stat3 = stats_from_array_data_ga("Benchmarking\GraphAlignment\\benchmark_ga_bk_3_0.5.txt", 3)
+    stat4 = stats_from_array_data_ga("Benchmarking\GraphAlignment\\benchmark_ga_bk_4_0.5.txt", 4)
+    stat5 = stats_from_array_data_ga("Benchmarking\GraphAlignment\\benchmark_ga_bk_5_0.5.txt", 5)
+    stat6 = stats_from_array_data_ga("Benchmarking\GraphAlignment\\benchmark_ga_bk_6_0.5.txt", 6)
+    stat7 = stats_from_array_data_ga("Benchmarking\GraphAlignment\\benchmark_ga_bk_7_0.5.txt", 7)
     x, y, e, n, failed = data_for_plot_from_stats_ga([stat3, stat4, stat5, stat6, stat7])
-    fig = build_bar_plot(x, y, e, n,  "Graph Alignment [Bron-Kerbosch]", 200, 175, "Time [s]")
+    fig = build_bar_plot_ga(x, y, e, n,  "Graph Alignment (Bron-Kerbosch)", 200, 175, "Time [s]")
     plt.savefig("GraphAlignment_BronKerbosch")
     plt.show()
     plt.close(fig)
 
     # Build plot for graph alignment using cordella
-    stat3 = stats_from_array_data("Benchmarking\GraphAlignment\\benchmark_ga_mb_3_0.5.txt", 3)
-    stat4 = stats_from_array_data("Benchmarking\GraphAlignment\\benchmark_ga_mb_4_0.5.txt", 4)
-    stat5 = stats_from_array_data("Benchmarking\GraphAlignment\\benchmark_ga_mb_5_0.5.txt", 5)
-    stat6 = stats_from_array_data("Benchmarking\GraphAlignment\\benchmark_ga_mb_6_0.5.txt", 6)
-    stat7 = stats_from_array_data("Benchmarking\GraphAlignment\\benchmark_ga_mb_7_0.5.txt", 7)
+    stat3 = stats_from_array_data_ga("Benchmarking\GraphAlignment\\benchmark_ga_mb_3_0.5.txt", 3)
+    stat4 = stats_from_array_data_ga("Benchmarking\GraphAlignment\\benchmark_ga_mb_4_0.5.txt", 4)
+    stat5 = stats_from_array_data_ga("Benchmarking\GraphAlignment\\benchmark_ga_mb_5_0.5.txt", 5)
+    stat6 = stats_from_array_data_ga("Benchmarking\GraphAlignment\\benchmark_ga_mb_6_0.5.txt", 6)
+    stat7 = stats_from_array_data_ga("Benchmarking\GraphAlignment\\benchmark_ga_mb_7_0.5.txt", 7)
     x, y, e, n, failed = data_for_plot_from_stats_ga([stat3, stat4, stat5, stat6, stat7])
     y = [i*1000 for i in y]
     e = [i*1000 for i in e]
-    fig = build_bar_plot(x, y, e, n, "Graph Alignment [VF2]", 3.5, 3, "Time [ms]")
+    fig = build_bar_plot_ga(x, y, e, n, "Graph Alignment (VF2)", 3.5, 3, "Time [ms]")
     plt.savefig("GraphAlignment_VF2")
     plt.show()
     plt.close()
 
-    """
+
     # Build plot for singly bron-kerbosch, assuming that error propagation is additive in relative errors (!)
-    stat_random = stats_from_array_data("Benchmarking\Bron-Kerbosch\\benchmark_bk_p_random.txt", 3)
-    stat_max = stats_from_array_data("Benchmarking\Bron-Kerbosch\\benchmark_bk_p_max.txt", 4)
-    data_for_plot_from_stats_bk(stat_random)
-    fig = build_bar_plot(x, y, e)
-    plt.savefig("GraphAlignment_BronKerbosch")
-    # plt.show()
+    stat_random = stats_from_array_data_bk("Benchmarking\Bron-Kerbosch\\benchmark_bk_p_random.txt", "Random")
+    stat_max = stats_from_array_data_bk("Benchmarking\Bron-Kerbosch\\benchmark_bk_p_max.txt", "Max")
+    x, y, e = data_for_plot_from_stats_bk([stat_random, stat_max])
+    y = [i * 1000 for i in y]
+    e = [i * 1000 for i in e]
+    fig = build_bar_plot_bk(x, y, e, 1.25, 1.1)
+    plt.savefig("BronKerbosch_Pivoting")
+    plt.show()
     plt.close()
-    """
