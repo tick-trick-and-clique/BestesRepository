11f844476f9fec3afa05587cb6bff8d96b996358
AJ-Felix <AJ-Felix@users.noreply.github.com>
2020-02-19 13:05:56 +0100

    quick fix error name too long master 2

--- Graph.py
+++ Graph.py
@@ -302,7 +302,6 @@ class GRAPH(object):
                 f.write("\n")
                 if self.__is_directed == False:
                     i = 0
-                    print("undirected")
                     for edge in self.__list_of_edges:
                         if i%2 == 0:
                             f.write("\n" + str(edge.get_start_and_end()[0].get_id()) + ";"
@@ -310,10 +309,6 @@ class GRAPH(object):
                             i+=1
                         else:
                             i+=1
-                else:
-                    for edge in self.__list_of_edges:
-                        f.write("\n" + str(edge.get_start_and_end()[0].get_id()) + ";"
-                                + str(edge.get_start_and_end()[1].get_id()) + ";" + str(edge.get_label()))
         f.close()
 
     def get_out_edge_list(self, vertex):
